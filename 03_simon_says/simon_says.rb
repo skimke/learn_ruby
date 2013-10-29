807545df3def087ec8b08e61946a567468e968ad
skimke <stella.kim@icloud.com>
2013-10-28 20:51:54 -0400

    Simon Says This thing WORKS

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,8 +1,30 @@
-def echo(greeting)
-  return "#{greeting}"
+def echo(phrase)
+  return "#{phrase}"
 end
 
-def shout(greeting)
-  greet = greeting.upcase
+def shout(phrase)
+  greet = phrase.upcase
   return greet
 end
+
+def repeat(phrase, num = 2)
+  copycat = ([phrase] * num).join " "
+  return copycat
+end
+
+def start_of_word(word, num = 0)
+  look_at = 0..(num-1)
+  start = word[look_at]
+  return start
+end
+
+def first_word(phrase)
+  return phrase[/\w+/]
+end
+
+def titleize(phrase)
+  words_avoid = ["and", "over", "the"]
+  words_inside = phrase.split
+  words_inside.each_with_index {|w, index| w.capitalize! unless words_avoid.include?(w) && index > 0 }.join " "
+end
+
