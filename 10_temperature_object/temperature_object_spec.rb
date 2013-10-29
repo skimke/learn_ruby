42fef782faf593d0624df06389eb5fa7093d01ba
skimke <stella.kim@icloud.com>
2013-10-29 17:12:30 -0400

    temperature.rb added

--- 10_temperature_object/temperature_object_spec.rb
+++ 10_temperature_object/temperature_object_spec.rb
@@ -5,7 +5,7 @@
 # Remember one degree fahrenheit is 5/9 of one degree celsius, 
 # and the freezing point of water is 0 degrees celsius but 32 degrees fahrenheit.
 
-require "temperature"
+require "./temperature"
 
 describe Temperature do
 
