450e9632a1363523d5aac03755a796c4d2f63ef5
skimke <stella.kim@icloud.com>
2013-10-29 17:11:15 -0400

    book titles dones minus last step

---
+++ 08_book_titles/timer.rb
@@ -0,0 +1,17 @@
+class Timer
+  def initialize(seconds = 0)
+    @seconds = seconds
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(num)
+    @seconds = seconds
+  end
+
+  def time_string
+    
+  end
+end
