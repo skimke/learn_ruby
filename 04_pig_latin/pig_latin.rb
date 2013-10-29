36fbe7a8db5e43d29e0fb61e152f6a0f66bd41bf
skimke <stella.kim@icloud.com>
2013-10-28 20:16:23 -0400

    update

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,7 +1,11 @@
-def translate(string)
-  if string[/\A[aeiou]/] != nil
-    string << "ay"
-  else 
-    string << "ay"
+def translate(phrase)
+  letters_avoid = ["a","e","i","o","u"]
+  letters_inside = phrase.split(//)
+  if letters_inside.include?(letters_avoid)
+    new_form = letters_inside.rotate
+    new_form = new_form.join << "ay"
+  else
+    new_form = letters_inside.join
+    new_form = new_form << "ay"
   end
 end
