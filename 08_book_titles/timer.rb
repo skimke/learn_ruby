1299db0e44cb5f40974122bd9d8b5d919e4f2a39
skimke <stella.kim@icloud.com>
2013-10-30 08:44:55 -0400

    completed

--- 08_book_titles/timer.rb
+++ 08_book_titles/timer.rb
@@ -1,4 +1,5 @@
 class Timer
+  attr_accessor :seconds
   def initialize(seconds)
     @seconds = 0
   end
@@ -8,6 +9,18 @@ class Timer
   end
 
   def time_string
-    puts "#{}:#{}:#{}"
+    hours = @seconds / 3600
+    minutes = @seconds % 3600 / 60
+    seconds = @seconds % 60
+
+    if seconds < 10
+      puts "0#{hours}:0#{minutes}:0#{seconds}"
+    elsif seconds > 10 && minutes > 10
+      puts "0#{hours}:0#{minutes}:0#{seconds}"
+    elsif minutes < 10
+      puts "0#{hours}:0#{minutes}:0#{seconds}"
+    elsif second < 10 && minutes < 10
+      puts "0#{hours}:0#{minutes}:0#{seconds}"
+    end
   end
 end
--- Exercise 2/rspec.rb
+++ Exercise 2/rspec.rb
@@ -1,13 +1,16 @@
 class Changer
 
-  COINS = %w(25 10 5 1)
+  def self.make_change(coins)
+  change = []
+  quarter = coins / 25
+  dime = (coins % 25) / 10
+  nickle = ((coins % 25) % 10) / 5
+  penny = ((coins % 25) % 10) % 5
 
-  QUARTER = 25
-  DIME = 10
-  NICKLE = 5
-  PENNY = 1
-
-  def self.make_change(num)
-    
+  quarter.times {change << 25}
+  dime.times {change << 10}
+  nickle.times {change << 5}
+  penny.times {change << 1}
+  return change
   end
 end
