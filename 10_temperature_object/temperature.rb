71a3ade9c53d04c02d78e905f5e3b0bae869bb47
skimke <stella.kim@icloud.com>
2013-10-29 20:51:01 -0400

    temperature half-way point

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,2 +1,25 @@
 class Temperature
+  def initialize(key)
+    @key = key
+  end
+
+  def to_fahrenheit
+    if @key[:c] 
+      @key[:c] * 9.0/5.0 + 32
+    else 
+      @key[:f]
+    end
+  end
+
+  def to_celsius
+    if @key[:f]
+      (@key[:f] - 32) * 5.0/9.0
+    else
+      @key[:c]
+    end
+  end
+
+  def self.in_celsius
+    return @key
+  end
 end
