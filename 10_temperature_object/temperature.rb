85b791ba0591a97e36c60f0b47db97f148a3e544
skimke <stella.kim@icloud.com>
2013-10-29 21:53:07 -0400

    temperature complete

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -19,7 +19,25 @@ class Temperature
     end
   end
 
-  def self.in_celsius
-    return @key
+  def self.in_celsius(val)
+    Temperature.new({:c => val})
+  end
+
+  def self.in_fahrenheit(val)
+    Temperature.new({:f => val})
   end
 end
+
+class Celsius < Temperature
+  def initialize(val)
+    super({:c => val})
+  end
+end
+
+class Fahrenheit < Temperature
+  def initialize(val)
+    super({:f => val})
+  end
+end
+
+# super calls back to the parent class for "help"
